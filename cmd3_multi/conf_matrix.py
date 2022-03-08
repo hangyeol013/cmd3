c7ac21b3a73c33c4567e91d65b4a403021680443
Kang <hangyeol013@gmail.com>
2022-03-08 23:33:29 +0100

    add feature fusion, conf matrix results

---
+++ cmd3_multi/conf_matrix.py
@@ -0,0 +1,38 @@
+import seaborn as sns
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+mode = 'multimodal'
+layer = 'pred_fusion'
+save_name = mode + '_' + layer
+exp_name = os.path.join(mode, layer)
+conf_path = 'results/' + exp_name + '/conf_matrix.csv'
+
+
+with open(conf_path) as f:
+    lines = f.read().splitlines()
+
+cf_matrix = []
+for line in lines:
+    cf_matrix.append(line.split(',')[1:])    
+
+cf_matrix = cf_matrix[1:]
+cf_matrix = np.array(cf_matrix).astype(int)
+
+
+
+ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues', fmt='d')
+
+ax.set_title('Seaborn Confusion Matrix with labels\n\n')
+ax.set_xlabel('\nPredicted Values')
+ax.set_ylabel('Actual Values ')
+
+## Ticket labels - List must be in alphabetical order
+ax.xaxis.set_ticklabels(['Stanley Kubrick','David Fincher', 'Joel Coen'])
+ax.yaxis.set_ticklabels(['Stanley Kubrick','David Fincher', 'Joel Coen'])
+
+## Display the visualization of the Confusion Matrix.
+save_path = 'conf_matrix/{}.png'.format(save_name)
+plt.savefig(save_path)
--- cmd3_multi/trainer.py
+++ cmd3_multi/trainer.py
@@ -32,12 +32,12 @@ def feature_fuse_train(vfeature_path, afeature_path, targets_path, ids_path):
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 
-    print(concat_features.shape)
-    print(targets.shape)
+    print('feature size: ', concat_features.shape)
+    print('target size: ', targets.shape)
 
-    train_loader = data.DataLoader(data.TensorDataset(concat_features, targets), batch_size=16)
+    train_loader = data.DataLoader(data.TensorDataset(concat_features, targets), batch_size=16, shuffle=True)
 
-    n_epochs = 5
+    n_epochs = 1
     for epoch in range(n_epochs):
         for batch in train_loader:
             
@@ -45,8 +45,8 @@ def feature_fuse_train(vfeature_path, afeature_path, targets_path, ids_path):
             inputs = inputs.cuda()
             targets = batch[1].cuda()
  
-            pred = model(inputs)
-            loss = loss_fn(pred, targets)
+            preds = model(inputs)
+            loss = loss_fn(preds, targets)
 
             optimizer.zero_grad()
             loss.backward()
@@ -55,12 +55,46 @@ def feature_fuse_train(vfeature_path, afeature_path, targets_path, ids_path):
             loss = loss.item()
             print('loss: ', loss)
 
-    torch.save(model.state_dict(), 'cmd3_multimodal.pth')
+    torch.save(model.state_dict(), 'cmd3_multi/cmd3_multimodal.pth')
 
 
-# def feature_fuse_evaluation(pretrained_path, data_path):
+def feature_fuse_eval(pretrained_path, vfeature_path, afeature_path, target_path, id_path):
 
-#     model = nn.Linear(concat_features.shape[1], NUM_CLASSES).cuda()
+    v_features = np.load(vfeature_path, allow_pickle=True)
+    a_features = np.load(afeature_path, allow_pickle=True)
+    concat_features = torch.cat((v_features, a_features), 1)
+
+    targets = np.load(target_path, allow_pickle=True)
+    ids = np.load(id_path, allow_pickle=True)
+
+    print(concat_features.shape)
+
+    model = nn.Linear(concat_features.shape[1], NUM_CLASSES).cuda()
+    pretrained_params = torch.load(pretrained_path)
+    model.load_state_dict(pretrained_params)
+
+    loader = data.DataLoader(data.TensorDataset(concat_features, targets), batch_size=16)
+    model.eval()
+
+    preds = []
+    preds_label = []
+
+    for batch in loader:
+        inputs = batch[0].float()
+        inputs = inputs.cuda()
+        target = batch[1].cuda()
+
+        with torch.no_grad():
+            pred = model(inputs)
+        preds.append(pred.cpu())
+        preds_label.append(np.argmax(pred.cpu(), axis=1))
+
+    preds = torch.cat(preds)
+    preds_label = torch.cat(preds_label)
+    
+    save_path = 'results/feature_fusion'
+    save_results(preds, preds_label, targets, ids, save_path)
+
 
 
 def pred_fuse(vPred_path, aPred_path, targets_path, ids_path):
@@ -74,6 +108,14 @@ def pred_fuse(vPred_path, aPred_path, targets_path, ids_path):
     alpha = 0.5
     preds_fusion = alpha * v_preds + (1-alpha) * a_preds
     preds_label = np.argmax(preds_fusion.cpu(), axis=1)
+
+    save_path = 'results/pred_fusion'
+
+    save_results(preds_fusion, preds_label, targets, ids, save_path)
+
+
+
+def save_results(preds, preds_label, targets, ids, save_path):
 
     file_results = {}
     id_set = list(set(ids))
@@ -103,19 +145,19 @@ def pred_fuse(vPred_path, aPred_path, targets_path, ids_path):
     file_accuracy = correct / total
 
     global_report = {}
-    global_report["global_accuracy"] = get_global_accuracy(preds_fusion, targets)
+    global_report["global_accuracy"] = get_global_accuracy(preds, targets)
     global_report['file_accuracy'] = file_accuracy
-    global_report["global_precision"] = get_global_precision(preds_fusion, targets)
-    global_report["global_recall"] = get_global_recall(preds_fusion, targets)
+    global_report["global_precision"] = get_global_precision(preds, targets)
+    global_report["global_recall"] = get_global_recall(preds, targets)
     global_report_df = pd.DataFrame(global_report, index=["global_clip"])
 
     class_report = {}
-    class_report["class_accuracy"] = get_class_accuracy(preds_fusion, targets)
-    class_report["class_precision"] = get_class_precision(preds_fusion, targets)
-    class_report["class_recall"] = get_class_recall(preds_fusion, targets)
+    class_report["class_accuracy"] = get_class_accuracy(preds, targets)
+    class_report["class_precision"] = get_class_precision(preds, targets)
+    class_report["class_recall"] = get_class_recall(preds, targets)
     class_report_df = pd.DataFrame(class_report)
 
-    confusion_matrix = get_confusion_matrix(preds_fusion, targets)
+    confusion_matrix = get_confusion_matrix(preds, targets)
     confusion_matrix_df = pd.DataFrame(confusion_matrix)
 
     results_report = {}
@@ -130,7 +172,7 @@ def pred_fuse(vPred_path, aPred_path, targets_path, ids_path):
     file_report_df = pd.DataFrame(file_report)
 
 
-    result_dir = 'results/pred_fusion'
+    result_dir = save_path
 
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
@@ -146,14 +188,23 @@ if __name__ == '__main__':
     
     fuse_mode = 'feature'
     
-    vfeatures_path = 'custom_features/video/cmd3_video_last_0305/features.pk'
-    afeatures_path = 'custom_features/audio/cmd3_audio_last_0305/features.pk'
+    # Train
+    vfeatures_train_path = 'custom_features/video/cmd3_video_last_trainfeature_0308/features.pk'
+    afeatures_train_path = 'custom_features/audio/cmd3_audio_last_trainfeature_0308/features.pk'
+    targets_train_path = 'custom_features/audio/cmd3_audio_last_trainfeature_0308/targets.pk'
+    ids_train_path = 'custom_features/audio/cmd3_audio_last_trainfeature_0308/ids.pk'
 
+    # Evaluation
+    pretrained_path = 'cmd3_multi/cmd3_multimodal.pth'
+    vfeatures_eval_path = 'custom_features/video/cmd3_video_last_0305/features.pk'
+    afeatures_eval_path = 'custom_features/audio/cmd3_audio_last_0305/features.pk'
+    targets_eval_path = 'custom_features/audio/cmd3_audio_last_0305/targets.pk'
+    ids_eval_path = 'custom_features/audio/cmd3_audio_last_0305/ids.pk'
+    
     vpreds_path = 'custom_features/video/cmd3_video_last_0305/preds.pk'
     apreds_path = 'custom_features/audio/cmd3_audio_last_0305/preds.pk'
 
-    targets_path = 'custom_features/audio/cmd3_audio_last_0305/targets.pk'
-    ids_path = 'custom_features/audio/cmd3_audio_last_0305/ids.pk'
     
-    feature_fuse_train(vfeatures_path, afeatures_path, targets_path, ids_path)
-    pred_fuse(vpreds_path, apreds_path, targets_path, ids_path)
+    feature_fuse_train(vfeatures_train_path, afeatures_train_path, targets_train_path, ids_train_path)
+    feature_fuse_eval(pretrained_path, vfeatures_eval_path, afeatures_eval_path, targets_eval_path, ids_eval_path)
+    # pred_fuse(vpreds_path, apreds_path, targets_path, ids_path)
