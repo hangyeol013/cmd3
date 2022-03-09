8e3961b66c74e5031d11c3478bf6d7bbd9930e36
Kang <hangyeol013@gmail.com>
2022-03-09 03:31:42 +0100

    conf matrix update

--- cmd3_multi/conf_matrix.py
+++ cmd3_multi/conf_matrix.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-mode = 'multimodal'
-layer = 'pred_fusion'
+mode = 'audio'
+layer = 'last_layer'
 save_name = mode + '_' + layer
 exp_name = os.path.join(mode, layer)
 conf_path = 'results/' + exp_name + '/conf_matrix.csv'
@@ -21,8 +21,7 @@ for line in lines:
 cf_matrix = cf_matrix[1:]
 cf_matrix = np.array(cf_matrix).astype(int)
 
-
-
+plt.figure(figsize = (9,9))
 ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues', fmt='d')
 
 ax.set_title('Seaborn Confusion Matrix with labels\n\n')
@@ -32,6 +31,8 @@ ax.set_ylabel('Actual Values ')
 ## Ticket labels - List must be in alphabetical order
 ax.xaxis.set_ticklabels(['Stanley Kubrick','David Fincher', 'Joel Coen'])
 ax.yaxis.set_ticklabels(['Stanley Kubrick','David Fincher', 'Joel Coen'])
+
+
 
 ## Display the visualization of the Confusion Matrix.
 save_path = 'conf_matrix/{}.png'.format(save_name)
